f8534910a9a42ee43d7311e20ca8d58be9391156
datadiskpfv <paul.valle@datadisk.co.uk>
2019-01-12 19:40:39 +0000

    looked at services, authentication, routing authentication, login and logout

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+  constructor(public id: number, public description: string, public done: boolean, public targetDate: Date) {
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,9 +14,9 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    { id: 1, description: 'Learn to program in Java'},
-    { id: 2, description: 'Learn to program in Javascript'},
-    { id: 3, description: 'Learn to program in Python'}
+    new Todo(1, 'Learn to program in Java', false, new Date()),
+    new Todo(2, 'Learn to program in Javascript', false, new Date()),
+    new Todo(3, 'Learn to program in Python', false, new Date())
     ];
 
   constructor() { }
