b7e41b50a75790c35596eb30944e89abbb19a5ab
datadiskpfv <paul.valle@datadisk.co.uk>
2019-01-13 11:59:30 +0000

    hooking application into a spring boot restful web service

---
+++ src/app/service/data/welcome-data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class WelcomeDataService {
+
+  constructor() { }
+
+  executeHelloWorldBeanService() {
+    console.log('Execute hello world bean service');
+  }
+}
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {WelcomeDataService} from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -12,10 +13,14 @@ export class WelcomeComponent implements OnInit {
   name = '';
 
   // ActivatedRoute - dependency injection
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private service: WelcomeDataService) { }
 
   ngOnInit() {
     // console.log(this.route.snapshot.params['name']);
     this.name = this.route.snapshot.params['name'];
+  }
+
+  getWelcomeMessage() {
+    this.service.executeHelloWorldBeanService();
   }
 }
