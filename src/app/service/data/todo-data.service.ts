fcd6ea408e0f76508ef3850f9462658099576140
pvalle <paul.valle@orbian.com>
2019-01-16 13:48:29 +0000

    update angular to use new JPA/H2 data

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Todo} from '../../list-todos/list-todos.component';
-import {API_URL} from '../../app.constants';
+import {API_URL, API_URL_JPA} from '../../app.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -18,11 +18,11 @@ export class TodoDataService {
   }
 
   retrieveAllTodos(username) {
-    return this.http.get<Todo[]>(`${API_URL}/users/${username}/todos`);
+    return this.http.get<Todo[]>(`${API_URL_JPA}/users/${username}/todos`);
   }
 
   retrieveTodo(username, id) {
-    return this.http.get<Todo>(`${API_URL}/users/${username}/todos/${id}`);
+    return this.http.get<Todo>(`${API_URL_JPA}/users/${username}/todos/${id}`);
   }
 
   updateTodo(username, id, todo) {
